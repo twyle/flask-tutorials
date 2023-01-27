bdb991d7352b6fce46dfb57c403e9dc7899598c4
lyle <okothlyleochieng@gmail.com>
2023-01-27 00:25:15 -0800

    implements active search

--- htmx/responsive-serach/simple/app.py
+++ htmx/responsive-serach/simple/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, render_template_string
 from models import User
 
 users = [
@@ -24,6 +24,22 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html', users=users)
+
+@app.route('/search/', methods=['POST'])
+def search():
+    templ = """
+            {% for user in users %}
+            <tr>
+                <td>{{ user.id }}</td>
+                <td>{{ user.fname }}</td>
+                <td>{{ user.lname }}</td>
+                <td>{{ user.email }}</td>
+            </tr>
+            {% endfor %}
+    """
+    searchWord = request.form.get('search', None)
+    matchusers = [user for user in users if user.search(searchWord)]
+    return render_template_string(templ, users=matchusers)
 
 
 if __name__ == '__main__':
